668474d75c01129f5309bf97c26ec2d7bfae2a99
Davide De Rosa <keeshux@gmail.com>
2018-09-21 19:53:38 +0200

    Indent negotiated parameters in log

--- TunnelKit/Sources/Core/SessionProxy.swift
+++ TunnelKit/Sources/Core/SessionProxy.swift
@@ -902,14 +902,14 @@ public class SessionProxy {
         
         let pushedFraming = pushReply.compressionFraming
         if let negFraming = pushedFraming {
-            log.info("Negotiated compression framing: \(negFraming.rawValue)")
+            log.info("\tNegotiated compression framing: \(negFraming.rawValue)")
         }
         if let negPing = pushReply.ping {
-            log.info("Negotiated keep-alive: \(negPing) seconds")
+            log.info("\tNegotiated keep-alive: \(negPing) seconds")
         }
         let pushedCipher = pushReply.cipher
         if let negCipher = pushedCipher {
-            log.info("Negotiated cipher: \(negCipher.rawValue)")
+            log.info("\tNegotiated cipher: \(negCipher.rawValue)")
         }
 
         let bridge: EncryptionBridge
