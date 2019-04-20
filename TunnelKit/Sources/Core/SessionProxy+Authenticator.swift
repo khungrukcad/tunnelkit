707db2c6ded1e9a36e578ceb9777edb54835fbad
Davide De Rosa <keeshux@gmail.com>
2019-04-20 17:20:45 +0200

    Add keydir to local options

--- TunnelKit/Sources/Core/SessionProxy+Authenticator.swift
+++ TunnelKit/Sources/Core/SessionProxy+Authenticator.swift
@@ -116,14 +116,8 @@ extension SessionProxy {
                     break
                 }
             }
-            if let strategy = options.tlsWrap?.strategy {
-                switch strategy {
-                case .auth:
-                    opts.append("tls-auth")
-                    
-                case .crypt:
-                    opts.append("tls-crypt")
-                }
+            if let direction = options.tlsWrap?.key.direction?.rawValue {
+                opts.append("keydir \(direction)")
             }
             let optsString = opts.joined(separator: ",")
             log.debug("TLS.auth: Local options: \(optsString)")
