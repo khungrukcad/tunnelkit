khungrukcad/tunnelkit

commit ee4a446fa5d29415769b0c99c568636516adde8b

Add new OpenVPN subspec
- AppExtension will need to be protocol-agnostic
- Adjust Errors.h access in LZO subspec

Fixes "umbrella header for module 'xxx' does not include header".

TunnelKit.podspec
@@ -12,7 +12,7 @@ Pod::Spec.new do |s|
     s.ios.deployment_target = "11.0"
     s.osx.deployment_target = "10.11"
 
-    s.default_subspecs = "Core", "AppExtension"
+    s.default_subspecs = "Core", "OpenVPN", "AppExtension"
 
     s.subspec "Core" do |p|
         p.source_files          = "TunnelKit/Sources/Core/**/*.{h,m,swift}"
@@ -26,20 +26,30 @@ Pod::Spec.new do |s|
         p.libraries = "resolv"
     end
 
+    s.subspec "OpenVPN" do |p|
+        p.source_files          = "TunnelKit/Sources/OpenVPN/**/*.{h,m,swift}"
+        p.private_header_files  = "TunnelKit/Sources/OpenVPN/**/*.h"
+        p.preserve_paths        = "TunnelKit/Sources/OpenVPN/*.modulemap"
+        p.pod_target_xcconfig   = { "SWIFT_INCLUDE_PATHS" => "${PODS_TARGET_SRCROOT}/TunnelKit/Sources/OpenVPN",
+                                    "APPLICATION_EXTENSION_API_ONLY" => "YES" }
+
+        p.dependency "TunnelKit/Core"
+    end
+
     s.subspec "AppExtension" do |p|
         p.source_files          = "TunnelKit/Sources/AppExtension/**/*.swift"
         p.frameworks            = "NetworkExtension"
         p.pod_target_xcconfig   = { "APPLICATION_EXTENSION_API_ONLY" => "YES" }
 
-        p.dependency "TunnelKit/Core"
-        p.dependency "SwiftyBeaver"
+        p.dependency "TunnelKit/OpenVPN"
     end
 
     s.subspec "LZO" do |p|
         p.source_files          = "TunnelKit/Sources/Core/LZO.h",
                                   "TunnelKit/Sources/Core/Errors.{h,m}",
-                                  "TunnelKit/Sources/LZO/**/*lzo*.{h,m,c}"
+                                  "TunnelKit/Sources/LZO/lib/*lzo*.{h,m,c}"
         p.private_header_files  = "TunnelKit/Sources/Core/LZO.h",
+                                  "TunnelKit/Sources/Core/Errors.h",
                                   "TunnelKit/Sources/LZO/lib/*lzo*.h"
         p.pod_target_xcconfig   = { "APPLICATION_EXTENSION_API_ONLY" => "YES" }
     end

commit a861c69f36abc77e66fe0887b96884261338113e

Fix missing paths in LZO subspec
StandardLZO.m was not matched.

Regression in ee4a446fa5d29415769b0c99c568636516adde8b

TunnelKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name              = "TunnelKit"
-    s.version           = "2.0.0"
+    s.version           = "2.0.1"
     s.summary           = "Non-official OpenVPN client for Apple platforms."
 
     s.homepage          = "https://github.com/passepartoutvpn/tunnelkit"
@@ -52,9 +52,11 @@ Pod::Spec.new do |s|
         t.subspec "LZO" do |p|
             p.source_files          = "TunnelKit/Sources/Core/LZO.h",
                                       "TunnelKit/Sources/Core/Errors.{h,m}",
+                                      "TunnelKit/Sources/Extra/LZO/*.{h,m}",
                                       "TunnelKit/Sources/Extra/LZO/lib/*lzo*.{h,m,c}"
             p.private_header_files  = "TunnelKit/Sources/Core/LZO.h",
                                       "TunnelKit/Sources/Core/Errors.h",
+                                      "TunnelKit/Sources/Extra/LZO/*.h",
                                       "TunnelKit/Sources/Extra/LZO/lib/*lzo*.h"
             p.pod_target_xcconfig   = { "APPLICATION_EXTENSION_API_ONLY" => "YES" }
         end

TunnelKit.xcodeproj/project.pbxproj
@@ -1716,7 +1716,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				CODE_SIGN_IDENTITY = "iPhone Developer";
 				COPY_PHASE_STRIP = NO;
-				CURRENT_PROJECT_VERSION = 567;
+				CURRENT_PROJECT_VERSION = 618;
 				DEBUG_INFORMATION_FORMAT = dwarf;
 				ENABLE_STRICT_OBJC_MSGSEND = YES;
 				ENABLE_TESTABILITY = YES;
@@ -1780,7 +1780,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				CODE_SIGN_IDENTITY = "iPhone Developer";
 				COPY_PHASE_STRIP = NO;
-				CURRENT_PROJECT_VERSION = 567;
+				CURRENT_PROJECT_VERSION = 618;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				ENABLE_NS_ASSERTIONS = NO;
 				ENABLE_STRICT_OBJC_MSGSEND = YES;
@@ -1813,7 +1813,7 @@
 				DEFINES_MODULE = YES;
 				DEVELOPMENT_TEAM = "";
 				DYLIB_COMPATIBILITY_VERSION = 1;
-				DYLIB_CURRENT_VERSION = 567;
+				DYLIB_CURRENT_VERSION = 618;
 				DYLIB_INSTALL_NAME_BASE = "@rpath";
 				FRAMEWORK_SEARCH_PATHS = "$(inherited)";
 				INFOPLIST_FILE = "$(SRCROOT)/TunnelKit-iOS/Info.plist";
@@ -1837,7 +1837,7 @@
 				DEFINES_MODULE = YES;
 				DEVELOPMENT_TEAM = "";
 				DYLIB_COMPATIBILITY_VERSION = 1;
-				DYLIB_CURRENT_VERSION = 567;
+				DYLIB_CURRENT_VERSION = 618;
 				DYLIB_INSTALL_NAME_BASE = "@rpath";
 				FRAMEWORK_SEARCH_PATHS = "$(inherited)";
 				INFOPLIST_FILE = "$(SRCROOT)/TunnelKit-iOS/Info.plist";
@@ -1861,7 +1861,7 @@
 				COMBINE_HIDPI_IMAGES = YES;
 				DEFINES_MODULE = YES;
 				DYLIB_COMPATIBILITY_VERSION = 1;
-				DYLIB_CURRENT_VERSION = 567;
+				DYLIB_CURRENT_VERSION = 618;
 				DYLIB_INSTALL_NAME_BASE = "@rpath";
 				FRAMEWORK_VERSION = A;
 				INFOPLIST_FILE = "$(SRCROOT)/TunnelKit-macOS/Info.plist";
@@ -1885,7 +1885,7 @@
 				COMBINE_HIDPI_IMAGES = YES;
 				DEFINES_MODULE = YES;
 				DYLIB_COMPATIBILITY_VERSION = 1;
-				DYLIB_CURRENT_VERSION = 567;
+				DYLIB_CURRENT_VERSION = 618;
 				DYLIB_INSTALL_NAME_BASE = "@rpath";
 				FRAMEWORK_VERSION = A;
 				INFOPLIST_FILE = "$(SRCROOT)/TunnelKit-macOS/Info.plist";

TunnelKitHost/Info.plist
@@ -15,7 +15,7 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>2.0.0</string>
+	<string>2.0.1</string>
 	<key>CFBundleVersion</key>
 	<string>1</string>
 	<key>LSRequiresIPhoneOS</key>

TunnelKitTests-iOS/Info.plist
@@ -15,7 +15,7 @@
 	<key>CFBundlePackageType</key>
 	<string>BNDL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>2.0.0</string>
+	<string>2.0.1</string>
 	<key>CFBundleVersion</key>
 	<string>1</string>
 </dict>

TunnelKitTests-macOS/Info.plist
@@ -15,7 +15,7 @@
 	<key>CFBundlePackageType</key>
 	<string>BNDL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>2.0.0</string>
+	<string>2.0.1</string>
 	<key>CFBundleVersion</key>
 	<string>1</string>
 </dict>